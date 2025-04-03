7941abdde26c0d4bf0620b09850f8f6e939dd736
Vinicius Solon <vini.97@live.com>
2025-04-03 18:32:23 -0300

    Start!

---
+++ src/main/java/org/vinisolon/internationalization/app/InternationalizationExampleApplication.java
@@ -0,0 +1,13 @@
+package org.vinisolon.internationalization.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalizationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalizationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vinisolon/internationalization/app/InternationalizationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vinisolon.internationalization.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalizationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
