c6b128ebff32321bec74fda94e23e31d720bfd98
Vinicius Solon <vini.97@live.com>
2025-04-05 07:15:36 -0300

    Changed business exception access modifiers

--- src/main/java/org/vinisolon/internationalization/app/internationalization/exception/MyBusinessException.java
+++ src/main/java/org/vinisolon/internationalization/app/internationalization/exception/MyBusinessException.java
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.vinisolon.internationalization.app.internationalization.domain.MyDomainMessageKeyEnum;
 
 @Getter
-public class MyBusinessException extends RuntimeException {
+public abstract class MyBusinessException extends RuntimeException {
 
     private final HttpStatus httpStatus;
 
-    public MyBusinessException(MyDomainMessageKeyEnum messageKey) {
+    protected MyBusinessException(MyDomainMessageKeyEnum messageKey) {
         super(messageKey.getKey());
         this.httpStatus = messageKey.getHttpStatus();
     }
